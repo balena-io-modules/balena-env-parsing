13bd8c181b0fcbbcaea4973f9920138097d12c3d
Pagan Gazzard <page@balena.io>
2022-08-12 16:44:31 +0100

    Add support for passing an array of vars as well as a single one
    Change-type: minor

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,22 @@ export const SECONDS_PER_HOUR = 60 * 60;
  */
 export const SECONDS = 1000;
 /**
-* In milliseconds
-*/
+ * In milliseconds
+ */
 export const MINUTES = 60 * SECONDS;
 /**
-* In milliseconds
-*/
+ * In milliseconds
+ */
 export const HOURS = 60 * MINUTES;
 /**
-* In milliseconds
-*/
+ * In milliseconds
+ */
 export const DAYS = 24 * HOURS;
 
 /**
  * Ensure an env var exists and return the contents if it does
  */
-export const requiredVar = (varName: string): string => {
+export const requiredVar = (varName: string | string[]): string => {
 	const s = optionalVar(varName);
 	if (s == null) {
 		throw new Error(`Missing environment variable: ${varName}`);
@@ -31,20 +31,35 @@ export const requiredVar = (varName: string): string => {
  * Fetches the contents of an env var if it exists and is not empty, otherwise returning the specified default value,
  * this means that if no default is specified then `''` will be normalized to `undefined`
  */
-export function optionalVar<R>(varName: string, defaultValue: R): string | R;
 export function optionalVar<R>(
-	varName: string,
+	varName: string | string[] | string[],
+	defaultValue: R,
+): string | R;
+export function optionalVar<R>(
+	varName: string | string[] | string[],
 	defaultValue?: undefined,
 ): string | undefined;
 export function optionalVar<R>(
-	varName: string,
+	varName: string | string[] | string[],
 	defaultValue?: R,
 ): string | R | undefined {
-	const s = process.env[varName];
-	if (s == null || s === '') {
-		return defaultValue;
+	// If there's a single string var name then fetch it directly from process.env, returning the default as necessary
+	if (typeof varName === 'string') {
+		const s = process.env[varName];
+		if (s == null || s === '') {
+			return defaultValue;
+		}
+		return s;
 	}
-	return s;
+
+	// Otherwise for a list then we loop through and return the first valid one, otherwise we return the default value
+	for (const name of varName) {
+		const s = optionalVar(name);
+		if (s != null) {
+			return s;
+		}
+	}
+	return defaultValue;
 }
 
 /**
@@ -66,9 +81,15 @@ export const checkInt = (s: string): number | undefined => {
  * Fetches the contents of an env var, ensuring it is a valid integer if set,
  * if it is not set then instead it returns the specified default value
  */
-export function intVar(varName: string): number;
-export function intVar<R>(varName: string, defaultValue: R): number | R;
-export function intVar<R>(varName: string, defaultValue?: R): number | R {
+export function intVar(varName: string | string[]): number;
+export function intVar<R>(
+	varName: string | string[],
+	defaultValue: R,
+): number | R;
+export function intVar<R>(
+	varName: string | string[],
+	defaultValue?: R,
+): number | R {
 	if (arguments.length === 1) {
 		requiredVar(varName);
 	}
@@ -89,9 +110,15 @@ export function intVar<R>(varName: string, defaultValue?: R): number | R {
  * Fetches the contents of an env var, ensuring it is a valid boolean (`'true'`/`'false'`) if set,
  * if it is not set then instead it returns the specified default value
  */
-export function boolVar(varName: string): boolean;
-export function boolVar<R>(varName: string, defaultValue: R): boolean | R;
-export function boolVar<R>(varName: string, defaultValue?: R): boolean | R {
+export function boolVar(varName: string | string[]): boolean;
+export function boolVar<R>(
+	varName: string | string[],
+	defaultValue: R,
+): boolean | R;
+export function boolVar<R>(
+	varName: string | string[],
+	defaultValue?: R,
+): boolean | R {
 	if (arguments.length === 1) {
 		requiredVar(varName);
 	}
@@ -117,7 +144,7 @@ export type HostPort = { host: string; port: number };
  * into an array of individual host/port pairings
  */
 export const hostPortsVar = (
-	varName: string,
+	varName: string | string[],
 	defaultHosts?: HostPort[],
 ): HostPort[] => {
 	const hostPairs = optionalVar(varName);
@@ -143,7 +170,7 @@ export const hostPortsVar = (
  * so that express will be able to handle those correctly
  */
 export const trustProxyVar = <R>(
-	varName: string,
+	varName: string | string[],
 	defaultValue?: R,
 ): boolean | string | number | R => {
 	const trustProxy = optionalVar(varName);
--- test/index.ts
+++ test/index.ts
@@ -10,7 +10,12 @@ import {
 	trustProxyVar,
 } from '../src';
 
-const varName = 'test';
+const varName = 'fruit';
+const varNames = ['vegetable', 'fruit'];
+const varTests = [
+	['single var name', varName],
+	['multi var name', varNames],
+] as const;
 const clearVar = () => {
 	delete process.env[varName];
 };
@@ -22,185 +27,189 @@ beforeEach(() => {
 	clearVar();
 });
 
-describe('requiredVar', () => {
-	it('should throw when the env var is missing', () => {
-		expect(() => requiredVar(varName)).to.throw;
-	});
-	it('should return the value when the env var exists', () => {
-		const value = 'apple';
-		setVar(value);
-		expect(requiredVar(varName)).to.equal(value);
-	});
-});
+for (const [testType, varTest] of varTests) {
+	describe(testType, () => {
+		describe('requiredVar', () => {
+			it('should throw when the env var is missing', () => {
+				expect(() => requiredVar(varTest)).to.throw;
+			});
+			it('should return the value when the env var exists', () => {
+				const value = 'apple';
+				setVar(value);
+				expect(requiredVar(varTest)).to.equal(value);
+			});
+		});
 
-describe('optionalVar', () => {
-	const defaultValue = 'banana';
-	it('should return undefined when the env var is missing', () => {
-		expect(optionalVar(varName)).to.be.undefined;
-	});
-	it('should return undefined when the env var is empty', () => {
-		setVar('');
-		expect(optionalVar(varName)).to.be.undefined;
-	});
-	it('should return the default value when the env var is missing', () => {
-		expect(optionalVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the default value when the env var is empty', () => {
-		setVar('');
-		expect(optionalVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the value when the env var exists', () => {
-		const value = 'coconut';
-		setVar(value);
-		expect(optionalVar(varName)).to.equal(value);
-		expect(optionalVar(varName, defaultValue)).to.equal(value);
-	});
-});
+		describe('optionalVar', () => {
+			const defaultValue = 'banana';
+			it('should return undefined when the env var is missing', () => {
+				expect(optionalVar(varTest)).to.be.undefined;
+			});
+			it('should return undefined when the env var is empty', () => {
+				setVar('');
+				expect(optionalVar(varTest)).to.be.undefined;
+			});
+			it('should return the default value when the env var is missing', () => {
+				expect(optionalVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the default value when the env var is empty', () => {
+				setVar('');
+				expect(optionalVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the value when the env var exists', () => {
+				const value = 'coconut';
+				setVar(value);
+				expect(optionalVar(varTest)).to.equal(value);
+				expect(optionalVar(varTest, defaultValue)).to.equal(value);
+			});
+		});
 
-describe('intVar', () => {
-	const defaultValue = 'banana';
-	it('should throw when the env var is missing', () => {
-		expect(() => intVar(varName)).to.throw;
-	});
-	it('should throw when the env var is invalid', () => {
-		setVar('date');
-		expect(() => intVar(varName)).to.throw;
-	});
-	it('should return the default value when the env var is missing', () => {
-		expect(intVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the default value when the env var is empty', () => {
-		setVar('');
-		expect(intVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the value when the env var is a valid number', () => {
-		const value = 123;
-		setVar(`${value}`);
-		expect(intVar(varName)).to.equal(value);
-		expect(intVar(varName, defaultValue)).to.equal(value);
-	});
-});
+		describe('intVar', () => {
+			const defaultValue = 'banana';
+			it('should throw when the env var is missing', () => {
+				expect(() => intVar(varTest)).to.throw;
+			});
+			it('should throw when the env var is invalid', () => {
+				setVar('date');
+				expect(() => intVar(varTest)).to.throw;
+			});
+			it('should return the default value when the env var is missing', () => {
+				expect(intVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the default value when the env var is empty', () => {
+				setVar('');
+				expect(intVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the value when the env var is a valid number', () => {
+				const value = 123;
+				setVar(`${value}`);
+				expect(intVar(varTest)).to.equal(value);
+				expect(intVar(varTest, defaultValue)).to.equal(value);
+			});
+		});
 
-describe('boolVar', () => {
-	const defaultValue = 'banana';
-	it('should throw when the env var is missing', () => {
-		expect(() => boolVar(varName)).to.throw;
-	});
-	it('should throw when the env var is invalid', () => {
-		setVar('date');
-		expect(() => boolVar(varName)).to.throw;
-	});
-	it('should return the default value when the env var is missing', () => {
-		expect(boolVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the default value when the env var is empty', () => {
-		setVar('');
-		expect(boolVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the value when the env var is true', () => {
-		const value = true;
-		setVar(`${value}`);
-		expect(boolVar(varName)).to.equal(value);
-		expect(boolVar(varName, defaultValue)).to.equal(value);
-	});
-	it('should return the value when the env var is false', () => {
-		const value = false;
-		setVar(`${value}`);
-		expect(boolVar(varName)).to.equal(value);
-		expect(boolVar(varName, defaultValue)).to.equal(value);
-	});
-});
+		describe('boolVar', () => {
+			const defaultValue = 'banana';
+			it('should throw when the env var is missing', () => {
+				expect(() => boolVar(varTest)).to.throw;
+			});
+			it('should throw when the env var is invalid', () => {
+				setVar('date');
+				expect(() => boolVar(varTest)).to.throw;
+			});
+			it('should return the default value when the env var is missing', () => {
+				expect(boolVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the default value when the env var is empty', () => {
+				setVar('');
+				expect(boolVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the value when the env var is true', () => {
+				const value = true;
+				setVar(`${value}`);
+				expect(boolVar(varTest)).to.equal(value);
+				expect(boolVar(varTest, defaultValue)).to.equal(value);
+			});
+			it('should return the value when the env var is false', () => {
+				const value = false;
+				setVar(`${value}`);
+				expect(boolVar(varTest)).to.equal(value);
+				expect(boolVar(varTest, defaultValue)).to.equal(value);
+			});
+		});
 
-describe('splitHostPort', () => {
-	const defaultValue: HostPort[] = [];
-	it('should throw when the env var is missing', () => {
-		expect(() => hostPortsVar(varName)).to.throw;
-	});
-	it('should throw when the env var is invalid', () => {
-		setVar('date');
-		expect(() => hostPortsVar(varName)).to.throw;
-	});
-	it('should return the default value when the env var is missing', () => {
-		expect(hostPortsVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the default value when the env var is empty', () => {
-		setVar('');
-		expect(hostPortsVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the value when the env var is valid', () => {
-		const value = [
-			{ host: '127.0.0.1', port: 8080 },
-			{ host: '10.0.0.1', port: 80 },
-		];
-		setVar('127.0.0.1:8080, 10.0.0.1:80');
-		expect(hostPortsVar(varName)).to.deep.equal(value);
-		expect(hostPortsVar(varName, defaultValue)).to.deep.equal(value);
-	});
-});
+		describe('splitHostPort', () => {
+			const defaultValue: HostPort[] = [];
+			it('should throw when the env var is missing', () => {
+				expect(() => hostPortsVar(varTest)).to.throw;
+			});
+			it('should throw when the env var is invalid', () => {
+				setVar('date');
+				expect(() => hostPortsVar(varTest)).to.throw;
+			});
+			it('should return the default value when the env var is missing', () => {
+				expect(hostPortsVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the default value when the env var is empty', () => {
+				setVar('');
+				expect(hostPortsVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the value when the env var is valid', () => {
+				const value = [
+					{ host: '127.0.0.1', port: 8080 },
+					{ host: '10.0.0.1', port: 80 },
+				];
+				setVar('127.0.0.1:8080, 10.0.0.1:80');
+				expect(hostPortsVar(varTest)).to.deep.equal(value);
+				expect(hostPortsVar(varTest, defaultValue)).to.deep.equal(value);
+			});
+		});
 
-describe('trustProxyVar', () => {
-	const defaultValue = false;
-	it('should throw when the env var is missing', () => {
-		expect(() => trustProxyVar(varName)).to.throw;
-	});
-	it('should throw when the env var is invalid', () => {
-		setVar('date');
-		expect(() => trustProxyVar(varName)).to.throw;
-	});
-	it('should return the default value when the env var is missing', () => {
-		expect(trustProxyVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should throw when the default var is invalid', () => {
-		setVar('date');
-		expect(() => trustProxyVar(varName, '')).to.throw;
-	});
-	it('should return the default value when the env var is empty', () => {
-		setVar('');
-		expect(trustProxyVar(varName, defaultValue)).to.equal(defaultValue);
-	});
-	it('should return the value when the env var is a valid number', () => {
-		const value = 123;
-		setVar(`${value}`);
-		expect(trustProxyVar(varName)).to.equal(value);
-		expect(trustProxyVar(varName, defaultValue)).to.equal(value);
-	});
-	it('should return the value when the env var is true', () => {
-		const value = true;
-		setVar(`${value}`);
-		expect(trustProxyVar(varName)).to.equal(value);
-		expect(trustProxyVar(varName, defaultValue)).to.equal(value);
-	});
-	it('should return the value when the env var is false', () => {
-		const value = false;
-		setVar(`${value}`);
-		expect(trustProxyVar(varName)).to.equal(value);
-		expect(trustProxyVar(varName, defaultValue)).to.equal(value);
-	});
-	it('should return the value when the env var is valid', () => {
-		const value = '127.0.0.1, 10.0.0.1';
-		setVar(value);
-		expect(trustProxyVar(varName)).to.deep.equal(value);
-		expect(trustProxyVar(varName, defaultValue)).to.deep.equal(value);
-	});
-});
+		describe('trustProxyVar', () => {
+			const defaultValue = false;
+			it('should throw when the env var is missing', () => {
+				expect(() => trustProxyVar(varTest)).to.throw;
+			});
+			it('should throw when the env var is invalid', () => {
+				setVar('date');
+				expect(() => trustProxyVar(varTest)).to.throw;
+			});
+			it('should return the default value when the env var is missing', () => {
+				expect(trustProxyVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should throw when the default var is invalid', () => {
+				setVar('date');
+				expect(() => trustProxyVar(varTest, '')).to.throw;
+			});
+			it('should return the default value when the env var is empty', () => {
+				setVar('');
+				expect(trustProxyVar(varTest, defaultValue)).to.equal(defaultValue);
+			});
+			it('should return the value when the env var is a valid number', () => {
+				const value = 123;
+				setVar(`${value}`);
+				expect(trustProxyVar(varTest)).to.equal(value);
+				expect(trustProxyVar(varTest, defaultValue)).to.equal(value);
+			});
+			it('should return the value when the env var is true', () => {
+				const value = true;
+				setVar(`${value}`);
+				expect(trustProxyVar(varTest)).to.equal(value);
+				expect(trustProxyVar(varTest, defaultValue)).to.equal(value);
+			});
+			it('should return the value when the env var is false', () => {
+				const value = false;
+				setVar(`${value}`);
+				expect(trustProxyVar(varTest)).to.equal(value);
+				expect(trustProxyVar(varTest, defaultValue)).to.equal(value);
+			});
+			it('should return the value when the env var is valid', () => {
+				const value = '127.0.0.1, 10.0.0.1';
+				setVar(value);
+				expect(trustProxyVar(varTest)).to.deep.equal(value);
+				expect(trustProxyVar(varTest, defaultValue)).to.deep.equal(value);
+			});
+		});
 
-describe('checkInt', () => {
-	it('should fail on an empty value', () => {
-		expect(checkInt('')).to.be.undefined;
+		describe('checkInt', () => {
+			it('should fail on an empty value', () => {
+				expect(checkInt('')).to.be.undefined;
+			});
+			it('should fail on a float', () => {
+				expect(checkInt('123.456')).to.be.undefined;
+			});
+			it('should fail on an exponent', () => {
+				expect(checkInt('1e3')).to.be.undefined;
+			});
+			it('should return the value on a valid integer', () => {
+				expect(checkInt('1000')).to.equal(1000);
+			});
+			it('should return the value on a valid negative integer', () => {
+				expect(checkInt('-987')).to.equal(-987);
+			});
+			it('should return the value on a valid integer with leading/trailing spaces', () => {
+				expect(checkInt(' 1000 ')).to.equal(1000);
+			});
+		});
 	});
-	it('should fail on a float', () => {
-		expect(checkInt('123.456')).to.be.undefined;
-	});
-	it('should fail on an exponent', () => {
-		expect(checkInt('1e3')).to.be.undefined;
-	});
-	it('should return the value on a valid integer', () => {
-		expect(checkInt('1000')).to.equal(1000);
-	});
-	it('should return the value on a valid negative integer', () => {
-		expect(checkInt('-987')).to.equal(-987);
-	});
-	it('should return the value on a valid integer with leading/trailing spaces', () => {
-		expect(checkInt(' 1000 ')).to.equal(1000);
-	});
-});
+}
